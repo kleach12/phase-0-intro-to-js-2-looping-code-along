78e0164db5452e8e6e7cfffa9289202da76fe675
Kale Leach <kleach1212@gmail.com>
2021-12-12 12:58:12 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+const event = ["surprise"]
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    message.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  };
+  return message;
+}
+
+const message = [] 
+
+function countDown(int) {
+  let i = 10;
+  while (i > -1) {
+    console.log (i--)
+  }
+}
+countDown;
